ca7ea655d32066c1ee055af500aaf06f45cca854
malay1803 <18acharyamalay@gmail.com>
2021-07-10 22:08:53 +0530

    removed the unsplash api as it was changing the photo on each render, now using source.unsplash.com/daily to change photo dynamically daily

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import unsplash from "./axios/unsplash";
 import Time from "./component/Time";
 import PositiveWords from "./component/PositiveWords";
 import AddFocus from "./component/AddFocus";
@@ -7,34 +6,11 @@ import "./App.css";
 import Quote from "./component/Quote";
 
 function App() {
-  const [photo, setPhoto] = useState([]);
-
-  const pageNo = Math.floor(Math.random() * 20) + 1;
-  const choseIMG = Math.floor(Math.random() * 10) + 1;
-
-  let getPhoto = async () => {
-    try {
-      const response = await unsplash.get("", {
-        params: {
-          page: pageNo,
-        },
-      });
-      const photoLink = response.data.results[choseIMG].urls.full;
-      setPhoto(photoLink);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getPhoto();
-  }, []);
-
   return (
     <div
       className="background"
       style={{
-        backgroundImage: `url(${photo})`,
+        backgroundImage: `url(https://source.unsplash.com/1920x1280/daily?nature)`,
       }}
     >
       <div className="container">
