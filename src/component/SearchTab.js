3c161e52a2373691fbddc6be3467033c8a53ea0f
malay1803 <18acharyamalay@gmail.com>
2021-07-12 01:17:42 +0530

    added stackoverflow in searchTab

--- src/component/SearchTab.js
+++ src/component/SearchTab.js
@@ -7,9 +7,13 @@ const SearchTab = () => {
     if (e.key === "Enter") {
       if (engine === "google") {
         window.open(`https://www.google.com/search?q=${e.target.value}`);
-      } else {
+      } else if(engine === "youtube") {
         window.open(
           `https://www.youtube.com/results?search_query=${e.target.value}`
+        );
+      } else{
+        window.open(
+          `https://stackoverflow.com/search?q=${e.target.value}`
         );
       }
     }
@@ -26,6 +30,7 @@ const SearchTab = () => {
       <select name="searchEngine" id="searchEngine" onChange={searchEngine}>
         <option value="google">&#xf1a0; Google </option>
         <option value="youtube">&#xf167; Youtube </option>
+        <option value="stackOverflow">&#xf16c; &nbsp;StackOverflow </option>
       </select>
     </div>
   );
